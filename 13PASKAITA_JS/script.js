a5292ff40ee2912bef8d39ff8eb3b26c05868803
eglesved <egle.svedarauskiene@gmail.com>
2024-10-08 16:45:19 +0300

    pradinis comitas

---
+++ 13PASKAITA_JS/script.js
@@ -0,0 +1,36 @@
+let name = "Eglė";
+console.log(name);
+
+let age = 20;
+console.log(age);
+
+const isAlive = true;
+console.log (isAlive);
+
+// 1 užduotis
+
+const shopName = "Maxima";
+const peopleCount = 300;
+const consentWithPrivacyPolicy = true;
+
+console.log(shopName);
+console.log(peopleCount);
+console.log(consentWithPrivacyPolicy);
+
+
+//If (sąlygos)
+
+let animalCount = 100;
+
+if(animalCount >50) {
+    console.log ("Zoologijos sode yra daug gyvūnų");
+} else {
+    console.log ("Zoologijos sode mažai gyvūnų");
+}
+let personAge =17;
+if (personAge < 18) {
+    console.log("Į klubą neįleidžiame");
+} else {
+    console.log ("Prašome užeiti");
+}
+
---
+++ 13_paskaita_Javascript/script.js
@@ -0,0 +1,67 @@
+console.log("hello my name is Eglė"); // This is my test code
+
+//console.log("Hello Mario"); 
+
+/*
+    ilgas komentaras
+    per kelias eilutes
+*/
+
+const firstName ="Eglė"; //tekstas (String)
+const lastName = "švedarauskienė";
+
+const fullName = firstName+ " "+ lastName;
+
+
+var dogName = "Zara";
+let age = 25;
+const catName = "Pablo";
+
+
+
+const discount = 0.5; //skaičius (Number)
+
+const isAdult = false; //neigiamas (Boolean)
+
+const isAlive = true; //teigiamas (Boolean)
+
+const itemX = null; //tuščia, nėra reikšmės
+
+const itemY = undefined; //nėra apibrėžta reikšmė
+
+
+console.log(fullName);
+let itemZ;
+console.log(itemZ);
+
+itemZ = "Vėliau priskirta reikšmė";
+console.log(itemZ);
+
+let itemA = null;
+console.log(itemA);
+
+
+//reference types
+
+//masyvai
+const array=[1, 2, 3];
+const names = ["Rokas","Tadas", "Tomas"];
+const fruits = ["Apple", "Bannana", "Kiwi"];
+
+console.log(fruits);
+
+
+//objektai
+const animalX = { name: "Zara", age: 3, color: "marble"};
+const animalY = {name: "Munis", age: 60, colot: "grey"};
+
+console.log(animalX);
+
+//objektų masyvai
+const items = [
+    { name: "Iphone 16", price: 1200, color: "black"},
+    { name: "Iphone 16 Pro ", price: 1400, color: "white"}
+];
+
+console.log(items);
+
